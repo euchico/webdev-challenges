740de6f60a604c29805ee4347968902e3627bfc0
euchico <franciscodepaula.web@gmail.com>
2020-07-28 21:33:39 -0300

    Criação da GH-Pages

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # webdev-challenges
 Desafios de Desenvolvimento Web
+
+## Template
+
+* [Album - Get Bootstrap](https://v5.getbootstrap.com/)
+* [Simple Sidebar - Start Bootstrap](https://startbootstrap.com/)
+
+## Contato
+
+Francisco de Paula (euchico) - <eu.francisco@outlook.com>
+
+## Licença
+
+[MIT License](https://github.com/euchico/euchico.github.io/blob/master/LICENSE.md)
---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+if (document.getElementById("course")){
+  document.getElementById("menu-toggle").addEventListener("click", openMenu);
+
+  function openMenu() {
+    let element = document.getElementById("content");
+    element.classList.toggle("show");
+  }
+
+  function copyContent(e) {
+    var copyText = document.getElementById(e);
+    var textArea = document.createElement("textarea");
+
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+
+    textArea.select();
+    document.execCommand("copy");
+
+    textArea.remove();
+  }
+}
